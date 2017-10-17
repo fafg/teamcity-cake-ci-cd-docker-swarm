3f514ae640503d435609081b8a2786b7f7b00a2f
Fabiano Arruda <fabiano.arruda@farfetch.com>
2017-10-17 09:19:34 +0100

    remove unused code
    Signed-off-by: Fabiano Arruda <fabiano.arruda@farfetch.com>

--- src/myapi/Controllers/AddController.cs
+++ src/myapi/Controllers/AddController.cs
@@ -45,30 +45,5 @@
 
             return Ok(responseMessage);
         }
-
-        // GET api/values
-        //[HttpGet]
-        //public IEnumerable<string> Get()
-        //{
-        //    return new string[] { "value1", "value2" };
-        //}
-
-        // POST api/values
-        //[HttpPost]
-        //public void Post([FromBody]string value)
-        //{
-        //}
-
-        // PUT api/values/5
-        //[HttpPut("{id}")]
-        //public void Put(int id, [FromBody]string value)
-        //{
-        //}
-
-        // DELETE api/values/5
-        //[HttpDelete("{id}")]
-        //public void Delete(int id)
-        //{
-        //}
     }
 }
